575ccc675120a93d0c44bc65ec047258fe12aa7b
rstockm2 <ross.stockman@optum.com>
2018-03-15 08:47:47 -0500

    permissions

--- load-jars.bat
+++
@@ -1,2 +0,0 @@
-call mvn install:install-file -Dfile=ojdbc7.jar -DgroupId=oracle -DartifactId=ojdbc7 -Dversion=12.1.0.1 -Dpackaging=jar
-call mvn install:install-file -Dfile=ojdbc8.jar -DgroupId=oracle -DartifactId=ojdbc8 -Dversion=12.2.0.1 -Dpackaging=jar
